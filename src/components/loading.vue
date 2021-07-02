3d10fac332f5cbb513dbe2edc606c0d6772c4887
Monti <ingmontij@gmail.com>
2021-07-02 20:50:33 -0300

    Adding bootstrap and transform HelloWorld component into loading component

---
+++ src/components/loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="center-loading">
+      <img src="@/assets/Loader.png" alt="Loading...">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  methods:{
+  },
+  mounted(){
+  }
+}
+</script>
+
+
+<style scoped>
+.center-loading {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Loading/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Loading from '@/components/loading.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Loading
   }
 }
 </script>
